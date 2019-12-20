2200a5a7aaf6558786b4ce26d2041f27f2348541
baspo <baspo@LAPTOP-S5FRG6DU>
2019-12-20 18:16:03 +0100

    complete calculationconstantk class

--- GUI 1.2/src/models/CalculatingConstantK.java
+++ GUI 1.2/src/models/CalculatingConstantK.java
@@ -2,5 +2,27 @@ package models;
 
 public class CalculatingConstantK {
 	private float constantK;
-	
+
+	public CalculatingConstantK(float constantK) {
+		super();
+		this.constantK = constantK;
+	}
+
+	public float getConstantK() {
+		return constantK;
+	}
+
+	public void setConstantK(float constantK) {
+		this.constantK = constantK;
+	}
+
+	@Override
+	public String toString() {
+		return "CalculatingConstantK [constantK=" + constantK + "]";
+	}
+	public void calculateK() {
+		float resultOfCalculation = 1/Cattle.getH();
+		this.constantK = resultOfCalculation;
+		
+	}
 }
